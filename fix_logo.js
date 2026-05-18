1d2ccf3027eb80660949d8f9ce9de1d0924fb8db
iatnav222 <tranvntai222@gmail.com>
2026-05-18 15:09:45 +0700

    cap nhat co le lan cuoi

--- fix_logo.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const files = ['products.html', 'detail.html', 'cart.html', 'checkout.html', 'login.html'];
-for (const file of files) {
-    let content = fs.readFileSync(file, 'utf8');
-    content = content.replace(/<i class="fa-solid fa-book-open"><\/i> BookHaven/g, '<img src="assets/images/logo/logo.png" alt="BookHaven Logo" style="height: 50px;">');
-    fs.writeFileSync(file, content, 'utf8');
-}
-console.log('Done fixing logo safely!');
